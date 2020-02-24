0fd1a7be7816137945230269fcab4ba07bd08044
tgoffinet23 <goffinettyler@yahoo.com>
2020-02-24 19:18:04 +0000

    Initial commit

---
+++ mario.js
@@ -0,0 +1,23 @@
+
+printPyramid(5);
+
+
+/*
+ * printPyramid
+ *
+ * Prints to the console a pyramid of '#' characters of the specified height
+ * For example, if height is 5, the console will look like this:
+ *          ##
+ *         ###
+ *        ####
+ *       #####
+ *      ######
+ */
+function printPyramid(height) {
+    console.log("Uh oh... the pyramid is under construction.");
+    console.log("Check back soon, our developers are hard at work as we speak!");
+
+    // TODO
+    // print that pyramid!
+
+}
